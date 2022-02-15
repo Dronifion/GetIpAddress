fad109dfeff4b6932b71a7bdbe8acc9c3f7f874f
Dronifion <dronifion@gmail.com>
2022-02-15 09:30:22 +0800

    Initial Commit

---
+++ IPAddress/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace IPAddress
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.lbDisplay = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 597);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "IP Address";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lbDisplay
+            // 
+            this.lbDisplay.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbDisplay.FormattingEnabled = true;
+            this.lbDisplay.Location = new System.Drawing.Point(0, 0);
+            this.lbDisplay.Name = "lbDisplay";
+            this.lbDisplay.Size = new System.Drawing.Size(550, 589);
+            this.lbDisplay.TabIndex = 1;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(93, 597);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(89, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Network Card";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(188, 597);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Clear";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(550, 632);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lbDisplay);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IP Addresses";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox lbDisplay;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ IPAddress/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//Application Used Library
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace IPAddress
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string hostname = Dns.GetHostName();
+            lbDisplay.Items.Add("Hostname: " + hostname);
+            System.Net.IPAddress[] addresses = Dns.GetHostAddresses(hostname);
+            foreach (System.Net.IPAddress addr in addresses)
+            {
+                lbDisplay.Items.Add("IP Address: " + addr.ToString() + " " + addr.AddressFamily);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            
+            System.Net.NetworkInformation.NetworkInterface[] nics = NetworkInterface.GetAllNetworkInterfaces();
+            foreach (NetworkInterface nic in nics)
+            {
+                //basically, there are just a bunch of properties to query
+                lbDisplay.Items.Add("ID: " + nic.Id);
+                lbDisplay.Items.Add("Name: " + nic.Name);
+                lbDisplay.Items.Add("Physical Address: " + nic.GetPhysicalAddress());
+                lbDisplay.Items.Add("IP Addresses: ");
+                foreach (UnicastIPAddressInformation addr in nic.GetIPProperties().UnicastAddresses)
+                {
+                    lbDisplay.Items.Add(addr.Address 
+                        //+ " " + DateTime.Now 
+                        + " (least expires " + new TimeSpan(0, 0, (int)addr.DhcpLeaseLifetime)
+                        +")"); //" - {0} (lease expires {1})",
+                }
+                lbDisplay.Items.Add(" ");
+            }
+        /*
+        // Only proceed if there is a network available.
+if (NetworkInterface.GetIsNetworkAvailable())
+{
+// Get the set of all NetworkInterface objects for the local
+// machine.
+NetworkInterface[] interfaces =
+NetworkInterface.GetAllNetworkInterfaces();
+        // Iterate through the interfaces and display information.
+foreach (NetworkInterface ni in interfaces)
+{
+// Report basic interface information.
+Console.WriteLine("Interface Name: {0}", ni.Name);
+Console.WriteLine(" Description: {0}", ni.Description);
+Console.WriteLine(" ID: {0}", ni.Id);
+Console.WriteLine(" Type: {0}", ni.NetworkInterfaceType);
+Console.WriteLine(" Speed: {0}", ni.Speed);
+Console.WriteLine(" Status: {0}", ni.OperationalStatus);
+// Report physical address.
+Console.WriteLine(" Physical Address: {0}",
+ni.GetPhysicalAddress().ToString());
+// Report network statistics for the interface.
+Console.WriteLine(" Bytes Sent: {0}",
+ni.GetIPv4Statistics().BytesSent);
+Console.WriteLine(" Bytes Received: {0}",
+ni.GetIPv4Statistics().BytesReceived);
+// Report IP configuration.
+Console.WriteLine(" IP Addresses:");
+foreach (UnicastIPAddressInformation addr
+in ni.GetIPProperties().UnicastAddresses)
+{
+Console.WriteLine(" - {0} (lease expires {1})",
+addr.Address,
+DateTime.Now +
+new TimeSpan(0, 0, (int)addr.DhcpLeaseLifetime));
+}
+Console.WriteLine(Environment.NewLine);
+}
+}
+else
+{
+Console.WriteLine("No network available.");
+}
+*/
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            lbDisplay.Items.Clear();
+        }
+
+        // Declare a method to handle NetworkAvailabilityChanged events.
+        private void NetworkAvailabilityChanged(
+        object sender, NetworkAvailabilityEventArgs e)
+        {
+            // Report whether the network is now available or unavailable.
+            if (e.IsAvailable)
+            {
+                lbDisplay.Items.Add("Network Available");
+            }
+            else
+            {
+                lbDisplay.Items.Add("Network Unavailable");
+            }
+        }
+
+        // Declare a method to handle NetworkAdressChanged events.
+        private void NetworkAddressChanged(object sender, EventArgs e)
+        {
+            lbDisplay.Items.Add("Current IP Addresses:");
+            // Iterate through the interfaces and display information.
+            foreach (NetworkInterface ni in
+            NetworkInterface.GetAllNetworkInterfaces())
+            {
+                foreach (UnicastIPAddressInformation addr
+                in ni.GetIPProperties().UnicastAddresses)
+                {
+                    lbDisplay.Items.Add(addr.Address
+                        + " - " + DateTime.Now 
+                        + "(lease expires " + new TimeSpan(0, 0, (int)addr.DhcpLeaseLifetime) 
+                        + ")");
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Add the handlers to the NetworkChange events.
+            NetworkChange.NetworkAvailabilityChanged +=
+            NetworkAvailabilityChanged;
+            NetworkChange.NetworkAddressChanged +=
+            NetworkAddressChanged;
+        }
+    }
+}
